23290215cb96779dc852795da20240b92741c04b
Adrian Lopez <adriiloma2005@gmail.com>
2025-02-17 10:42:23 +0100

    aaa

--- config/config.php
+++ config/config.php
@@ -9,11 +9,21 @@ define("DEFAULT_CONTROLLER", "Mujer");
 define("DEFAULT_ACTION", "List");
 */
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "172.20.227.241");
 define("DB", "skills_2023");
 define("DB_USER", "adrian");
 define("DB_PASS", "g]tHH[[iQC8oouw4");
 
 define("DEFAULT_CONTROLLER", "Mujer");
 define("DEFAULT_ACTION", "List");
+
+/*
+define("DB_HOST", "150.241.37.58");
+define("DB", "skills_2023");
+define("DB_USER", "adrian");
+define("DB_PASS", "g]tHH[[iQC8oouw4");
+
+define("DEFAULT_CONTROLLER", "Mujer");
+define("DEFAULT_ACTION", "List");
+*/
 ?>
